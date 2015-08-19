d719fb3037cc692f152c87c4d3dbc02b924a1993
Thomas Brodin <thomasbrodin@gmail.com>
2015-08-19 14:23:13 -0400

    WP Init

---
+++ README.md
@@ -0,0 +1,5 @@
+Zac Posen - Wordpress Theme
+===================================
+
+With Timber & [Twig Template Engine](http://twig.sensiolabs.org/)
+[Timber Documentation](https://github.com/jarednova/timber/wiki/)
---
+++ content/themes/ZacPosen/functions.php
@@ -0,0 +1,62 @@
+<?php
+
+if ( ! class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+		} );
+	return;
+}
+define('THEME_URL', get_template_directory_uri());
+
+class StarterSite extends TimberSite {
+
+	function __construct() {
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'menus' );
+		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_action('wp_enqueue_scripts', array($this, 'load_scripts'));
+		add_action('wp_enqueue_scripts', array($this, 'load_styles'));
+		add_action('init', array($this,  'removeHeadLinks'));
+		parent::__construct();
+	}
+
+	function load_scripts(){
+		wp_enqueue_script('jquery');
+		wp_enqueue_script( 'modernizr', THEME_URL . '/js/modernizr-2.8.3.min.js', array('jquery'), false, false);
+		wp_enqueue_script( 'main-compressed', THEME_URL . '/js/main-min.js', array('jquery'), '', true);
+	}
+
+	function load_styles() {
+		wp_enqueue_style( 'custom', THEME_URL . '/css/main.css', false, filemtime(get_stylesheet_directory() .'/css/main.css'));
+	}
+
+	function removeHeadLinks() {
+			remove_action('wp_head', 'rsd_link');
+			remove_action('wp_head', 'wlwmanifest_link');
+			remove_action('wp_head', 'wp_generator');
+			remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+			remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		}
+
+	function add_to_context( $context ) {
+		$context['menu'] = new TimberMenu();
+		$context['site'] = $this;
+		return $context;
+	}
+
+	function add_to_twig( $twig ) {
+		/* this is where you can add your own fuctions to twig */
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		$twig->addFilter( 'myfoo', new Twig_Filter_Function( 'myfoo' ) );
+		return $twig;
+	}
+
+}
+
+new StarterSite();
+
+function myfoo( $text ) {
+	$text .= ' bar!';
+	return $text;
+}
